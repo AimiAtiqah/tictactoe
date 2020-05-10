3c8bfca0b38a00224812fde1de759eab425f5744
AimiAtiqah <63786700+AimiAtiqah@users.noreply.github.com>
2020-05-10 23:39:47 +0800

    Add project files.

---
+++ tictactoe/Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace tictactoe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip2 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.AboutBtn = new System.Windows.Forms.ToolStripMenuItem();
+            this.ExitButton = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem6 = new System.Windows.Forms.ToolStripMenuItem();
+            this.R1C2 = new System.Windows.Forms.Button();
+            this.R1C1 = new System.Windows.Forms.Button();
+            this.R1C3 = new System.Windows.Forms.Button();
+            this.R2C2 = new System.Windows.Forms.Button();
+            this.R2C3 = new System.Windows.Forms.Button();
+            this.R2C1 = new System.Windows.Forms.Button();
+            this.R3C1 = new System.Windows.Forms.Button();
+            this.R3C2 = new System.Windows.Forms.Button();
+            this.R3C3 = new System.Windows.Forms.Button();
+            this.NewGameBtn = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip2
+            // 
+            this.menuStrip2.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.toolStripMenuItem2});
+            this.menuStrip2.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip2.Name = "menuStrip2";
+            this.menuStrip2.Size = new System.Drawing.Size(233, 28);
+            this.menuStrip2.TabIndex = 0;
+            this.menuStrip2.Text = "menuStrip2";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.AboutBtn,
+            this.NewGameBtn,
+            this.ExitButton});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(46, 24);
+            this.toolStripMenuItem1.Text = "File";
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem6});
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(55, 24);
+            this.toolStripMenuItem2.Text = "Help";
+            // 
+            // AboutBtn
+            // 
+            this.AboutBtn.Name = "AboutBtn";
+            this.AboutBtn.Size = new System.Drawing.Size(224, 26);
+            this.AboutBtn.Text = "About";
+            this.AboutBtn.Click += new System.EventHandler(this.toolStripMenuItem4_Click);
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(224, 26);
+            this.ExitButton.Text = "Exit";
+            this.ExitButton.Click += new System.EventHandler(this.toolStripMenuItem5_Click);
+            // 
+            // toolStripMenuItem6
+            // 
+            this.toolStripMenuItem6.Name = "toolStripMenuItem6";
+            this.toolStripMenuItem6.Size = new System.Drawing.Size(224, 26);
+            this.toolStripMenuItem6.Text = "About";
+            // 
+            // R1C2
+            // 
+            this.R1C2.Location = new System.Drawing.Point(90, 45);
+            this.R1C2.Name = "R1C2";
+            this.R1C2.Size = new System.Drawing.Size(59, 58);
+            this.R1C2.TabIndex = 1;
+            this.R1C2.UseVisualStyleBackColor = true;
+            this.R1C2.Click += new System.EventHandler(this.button_click);
+            // 
+            // R1C1
+            // 
+            this.R1C1.Location = new System.Drawing.Point(25, 45);
+            this.R1C1.Name = "R1C1";
+            this.R1C1.Size = new System.Drawing.Size(59, 58);
+            this.R1C1.TabIndex = 2;
+            this.R1C1.UseVisualStyleBackColor = true;
+            this.R1C1.Click += new System.EventHandler(this.button_click);
+            // 
+            // R1C3
+            // 
+            this.R1C3.Location = new System.Drawing.Point(155, 45);
+            this.R1C3.Name = "R1C3";
+            this.R1C3.Size = new System.Drawing.Size(59, 58);
+            this.R1C3.TabIndex = 3;
+            this.R1C3.UseVisualStyleBackColor = true;
+            this.R1C3.Click += new System.EventHandler(this.button_click);
+            // 
+            // R2C2
+            // 
+            this.R2C2.Location = new System.Drawing.Point(90, 109);
+            this.R2C2.Name = "R2C2";
+            this.R2C2.Size = new System.Drawing.Size(59, 58);
+            this.R2C2.TabIndex = 4;
+            this.R2C2.UseVisualStyleBackColor = true;
+            this.R2C2.Click += new System.EventHandler(this.button_click);
+            // 
+            // R2C3
+            // 
+            this.R2C3.Location = new System.Drawing.Point(155, 109);
+            this.R2C3.Name = "R2C3";
+            this.R2C3.Size = new System.Drawing.Size(59, 58);
+            this.R2C3.TabIndex = 5;
+            this.R2C3.UseVisualStyleBackColor = true;
+            this.R2C3.Click += new System.EventHandler(this.button_click);
+            // 
+            // R2C1
+            // 
+            this.R2C1.Location = new System.Drawing.Point(25, 109);
+            this.R2C1.Name = "R2C1";
+            this.R2C1.Size = new System.Drawing.Size(59, 58);
+            this.R2C1.TabIndex = 6;
+            this.R2C1.UseVisualStyleBackColor = true;
+            this.R2C1.Click += new System.EventHandler(this.button_click);
+            // 
+            // R3C1
+            // 
+            this.R3C1.Location = new System.Drawing.Point(25, 173);
+            this.R3C1.Name = "R3C1";
+            this.R3C1.Size = new System.Drawing.Size(59, 58);
+            this.R3C1.TabIndex = 7;
+            this.R3C1.UseVisualStyleBackColor = true;
+            this.R3C1.Click += new System.EventHandler(this.button_click);
+            // 
+            // R3C2
+            // 
+            this.R3C2.Location = new System.Drawing.Point(90, 173);
+            this.R3C2.Name = "R3C2";
+            this.R3C2.Size = new System.Drawing.Size(59, 58);
+            this.R3C2.TabIndex = 8;
+            this.R3C2.UseVisualStyleBackColor = true;
+            this.R3C2.Click += new System.EventHandler(this.button_click);
+            // 
+            // R3C3
+            // 
+            this.R3C3.Location = new System.Drawing.Point(155, 173);
+            this.R3C3.Name = "R3C3";
+            this.R3C3.Size = new System.Drawing.Size(59, 58);
+            this.R3C3.TabIndex = 9;
+            this.R3C3.UseVisualStyleBackColor = true;
+            this.R3C3.Click += new System.EventHandler(this.button_click);
+            // 
+            // NewGameBtn
+            // 
+            this.NewGameBtn.Name = "NewGameBtn";
+            this.NewGameBtn.Size = new System.Drawing.Size(224, 26);
+            this.NewGameBtn.Text = "New Game";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(233, 253);
+            this.Controls.Add(this.R3C3);
+            this.Controls.Add(this.R3C2);
+            this.Controls.Add(this.R3C1);
+            this.Controls.Add(this.R2C1);
+            this.Controls.Add(this.R2C3);
+            this.Controls.Add(this.R2C2);
+            this.Controls.Add(this.R1C3);
+            this.Controls.Add(this.R1C1);
+            this.Controls.Add(this.R1C2);
+            this.Controls.Add(this.menuStrip2);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.MainMenuStrip = this.menuStrip2;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.RightToLeftLayout = true;
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tic Tac Toe";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip2.ResumeLayout(false);
+            this.menuStrip2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newGameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.Button A1;
+        private System.Windows.Forms.Button A2;
+        private System.Windows.Forms.Button A3;
+        private System.Windows.Forms.Button B1;
+        private System.Windows.Forms.Button B2;
+        private System.Windows.Forms.Button B3;
+        private System.Windows.Forms.Button C3;
+        private System.Windows.Forms.Button C2;
+        private System.Windows.Forms.Button C1;
+        private System.Windows.Forms.MenuStrip menuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem AboutBtn;
+        private System.Windows.Forms.ToolStripMenuItem ExitButton;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem6;
+        private System.Windows.Forms.Button R1C2;
+        private System.Windows.Forms.Button R1C1;
+        private System.Windows.Forms.Button R1C3;
+        private System.Windows.Forms.Button R2C2;
+        private System.Windows.Forms.Button R2C3;
+        private System.Windows.Forms.Button R2C1;
+        private System.Windows.Forms.Button R3C1;
+        private System.Windows.Forms.Button R3C2;
+        private System.Windows.Forms.Button R3C3;
+        private System.Windows.Forms.ToolStripMenuItem NewGameBtn;
+    }
+}
+
---
+++ tictactoe/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tictactoe
+{
+    public partial class Form1 : Form
+    {
+        bool turn = true; //true = X turn, False = Y turn
+        int turn_count= 0;//initialize count for turns
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+              
+        private void toolStripMenuItem4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("By Aimi 10 May 2020", "Tic Tac Toe 1");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripMenuItem5_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button_click(object sender, EventArgs e)
+        {
+            //sender = b, object = button. passing sender object as the button
+            Button b = (Button)sender;
+
+            //when button is clicked
+            if (turn)
+                b.Text = "X"; //if its the person's turn, the text property of the button is X
+            else
+                b.Text = "O";//else, if not their turn, text will be O
+
+            turn = !turn;
+            //but when the button that's already been clicked, is clikced again, 
+            //it changed the present value in the box to the opposite value
+            //so, to control that:
+            b.Enabled = false; //so when the button already clicked, it will be disabled
+
+            checkforwin();//every loop will check if there is winner using the below function
+        }
+
+        private void checkforwin()
+        {
+            bool winnerishere = false;
+
+            //horizontal checking
+            if ((R1C1.Text == R1C2.Text) && (R1C2.Text == R1C3.Text) && (!R1C1.Enabled))//checks if R1C! is enabled. disabled means it si completed. can be any button in the row
+                winnerishere = true;
+            else if((R2C1.Text == R2C2.Text) && (R2C2.Text == R2C3.Text) && (!R2C1.Enabled))
+                winnerishere = true;
+            else if ((R3C1.Text == R3C2.Text) && (R3C2.Text == R3C3.Text) && (!R3C1.Enabled))
+                winnerishere = true;
+
+
+            if (winnerishere) //when winnerishere = true, then this is called
+            {
+                disableButtons();
+
+                string winner = "";
+                if (turn)
+                    winner = "O";
+                else
+                    winner = "X";
+
+                MessageBox.Show(winner + " Wins!", "Yay!");
+            }//end winnerishere
+        }//end checkforwin
+
+        private void disableButtons()
+        {
+            try
+            {
+                foreach (Control c in Controls) //for each button is our form1, we want to disable the buttons after there is winner
+                {
+                    Button b = (Button) c;
+                    b.Enabled = false;
+                }//end foreach
+
+            } //endtry
+
+            catch { }
+            
+        }//end disablebuttons
+    }
+}
